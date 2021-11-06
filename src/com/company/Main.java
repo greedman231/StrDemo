dc9b2dc61624bf60f9d8ac3200a2216f13b3693b
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-11-06 13:16:01 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Первая строка \n Вторая строка");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
